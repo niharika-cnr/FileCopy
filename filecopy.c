9fdc3f4e5f554c284ef33df1c314b5b3b267f89b
Niharika CNR <niharika-cnr@users.noreply.github.com>
2017-02-12 13:15:51 +0530

    Update filecopy.c

--- filecopy.c
+++ filecopy.c
@@ -1,73 +1,106 @@
-/* Name:
-	ID:
-	Teammate name(s):
-	Explain your steps as comments in the code itself.
+/*	Name:			Niharika CNR
+	ID: 			1510110234
+	Teammate name:	Sanjana Gautam
+	Team Number: 	9
 */
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
 
 
-#define BUFFER_SIZE 25
+#define MAX_SIZE 250
 #define READ_END 0
 #define WRITE_END 1
 
 
-int main(int argc, char **argv){
-	char write_msg[BUFFER_SIZE] = "Greetings\n";
-	char read_msg [BUFFER_SIZE];
+int main(int argc, char **argv)	{
+	char write_data[MAX_SIZE];
+	char read_data [MAX_SIZE];
+	
+	//character to read from the file
 	char ch;
+	//looping variable
+	int i=0;
+	
+	//File pointers for source and target files
+	FILE *src = NULL;
+	FILE *trgt= NULL;
 
 	int fd[2];
 	pid_t cpid;
+
+	/* char*'s to store the names of the files passed as command line arguments*/
+	char *sourceFileName = argv[1];
+	char *targetFileName = argv[2];
 	
-	char *sourceFile = argv[1];
-	char *targetFile = argv[2];
+
+	pipe(fd); /* creates pipe */
 	
-	FILE *trgt = fopen(targetFile,"w");
-	FILE *src  = fopen(sourceFile,"r");
-	
-	printf("\n %s %s \n\n",sourceFile,targetFile);
-	
-	while (1) {
-      ch = fgetc(fp1);
- 
-      if (ch == EOF)
-         break;
-      else
-         putc(ch, fp2);
-   }
-	
-	pipe(fd); /* create pipe */
 	if((cpid = fork()) == -1){
 		perror("Fork failed");
 		exit(1);
 	}
-	
-	if(cpid == 0){	// child process
+
+	// child process: child only READS
+	if(cpid==0){
 		/* close the unused end of the pipe */
 		close(fd[WRITE_END]);
-		/* read from the pipe */
-		read(fd[READ_END], read_msg, BUFFER_SIZE);
-		read(
-		printf("read: %s",read_msg);
+		
+		/* Create the destination file to write into if it does not exist */
+		trgt = fopen(targetFileName,"w");
+		
+		/* Open the destination file and read from pipe */
+		if(trgt!=NULL)	{
+			/* read from the pipe */
+			read(fd[READ_END], read_data, MAX_SIZE);
+			fprintf(trgt,"%s",read_data);
+			
+			printf("\nContents of %s have been copied into %s\n
+
+\n",sourceFileName,targetFileName);
+		}//end if
+		
+		/* Print error message if the target file does not exist */
+		else	{
+			printf("\nSorry! File could not be created!\n");
+		}
 		/* close the write end of the pipe */
 		close(fd[READ_END]);
 
-		/* Open the destination file and read from pipe */
-		/* Create the destination file if it does not exist */
 	}
-	else{		//parent process execution
-		/* parent only writes. Close the read end */
+	
+	//parent process execution: parent only WRITES	
+	else{
+		/* close the unused end of the pipe */
 		close(fd[READ_END]);
-		write(fd[WRITE_END], write_msg, strlen(write_msg)+1);
+		
+		/* Open the extant source file to read from */
+		src  = fopen(sourceFileName,"r");
+		
+		/* Open the source file and write to pipe */
+		if(src!=NULL)	{
+			while(1)	{
+				ch = fgetc(src);
+						 
+				if ( (ch==EOF) || i==MAX_SIZE )
+				   break;
+				else
+				   write_data[i++]=ch;
+			}//end while
+			
+			write(fd[WRITE_END], write_data, strlen(write_data)+1);
+		}//end if
+		
+		/* Print error message if the source file does not exist */
+		else	{
+			printf("\nSource File not found!\n");
+		}//end else
+		
 		/* close the write end of the pipe */
 		close(fd[WRITE_END]);
-
-		/* Open the source file and write to pipe */
-		/* Print error message if the source file does not exist */
 	}
-	/* Remember to close the files */
-
+	
 	return 0;
 }
